620978eaa54b3cb47f999a0ff239883ca981a4f6
MvCr0s <165018211+MvCr0s@users.noreply.github.com>
2025-09-11 11:52:59 +0200

    Delete Grado en Ingeniería Informática/Segundo Curso/Estructuras de Datos y Algoritmos/PRACTICA 1-defensa directory

--- Grado en Ingeniería Informática/Segundo Curso/Estructuras de Datos y Algoritmos/PRACTICA 1-defensa/Celda.java
+++
@@ -1,7 +0,0 @@
-//Hecho por Marcos de Diego y Alfredo del val
-public interface Celda{
-
-    public void Inicializar(int n);
-    public void RayoCosmico(int i,int j);
-    public boolean Cortocircuito();
-}
